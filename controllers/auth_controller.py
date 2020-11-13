37ac92e6d980a15637b1900a1973b3d90a235d57
Logan Harris <loganrodharris@gmail.com>
2020-11-13 11:51:53 +1100

    hotfix: syntax errors

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -4,6 +4,6 @@ from models.User import User
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
-@auth.route("/register", method=["POST"])
+@auth.route("/register", methods=["POST"])
 def auth_register():
     return "working"
